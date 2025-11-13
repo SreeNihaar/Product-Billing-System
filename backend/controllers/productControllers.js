024555954595bf0e61073dfde1d760ff5ce1cc09
SreeNihaar <sreenihaarchaturvedula04@gmail.com>
2025-11-13 22:26:18 +0530

    Changed Database and added new router

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,11 +1,33 @@
-import {sql} from "../config/db.js";
+import supabase from "../config/db.js";
 
 export const getAllProducts = async (req, res)=>{
     try{
-        const response = await sql`
-            SELECT * from products;
-        `;
-        return res.status(200).json(response);
+        const {data,error} = await supabase.from("products").select('*');
+        if(error){
+            console.log(error);
+            return res.status(500).json({msg: error.message,status: 500});
+        }
+        res.status(200).json(data);
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({msg: error.message,status: 500});
+    }
+};
+
+export const getFilteredProducts = async (req,res)=>{
+    try{
+        const low = req.query.low,high=req.query.high;
+        const {data,error} = await supabase
+                                        .from("products")
+                                        .select()
+                                        .gte('price',low)
+                                        .lte('price',high);
+        if(error){
+            console.log(error);
+            return res.status(500).json({msg: error.message,status: 500});
+        }    
+        return res.status(200).json(data);
     }
     catch(error){
         console.log(error);
